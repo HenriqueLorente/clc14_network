9dbf6835608c1cd1e3d7292483bb08b7a186e571
Henrique Lorente <ec2-user@ip-172-31-19-35.ec2.internal>
2025-11-25 23:25:08 +0000

    arquivos terraform

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket = "henrique-terraform-clc14-automation"
+    key    = "network/terraform.tfstate"
+    region = "us-east-1"
+    use_lockfile = true
+  }
+}
